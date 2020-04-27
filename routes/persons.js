01bccef329049bc71d6c4939e715aeda8f6a96a1
Pharsa Thapa <pharsathapa@pharsas-mbp.home>
2020-04-27 17:53:36 +1000

    added put function for person

--- routes/persons.js
+++ routes/persons.js
@@ -92,7 +92,7 @@ router.get("/", function(req, res, next){
 router.get("/:id", function(req, res, next){
     let getParam = { _id : req.params.id } ;
     if (req.organisationId !== undefined ) { getParam.organisation = req.organisationId}
-    personModel.find(getParam, '-__v' ,function(err, person) {
+    personModel.findOne(getParam, '-__v' ,function(err, person) {
         if (err) {
             res.json({"error" : "Internal Sever Error" + err});
         } else {
@@ -105,11 +105,54 @@ router.get("/:id", function(req, res, next){
     });
 });
 
+/**
+ * @swagger
+ * path:
+ *  /persons/{personId}:
+ *    put:
+ *      summary: update a person by id
+ *      tags: [Persons]
+ *      parameters:
+ *        - in: path
+ *          name: personId
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: Id of the person
+ *      responses:
+ *        "200":
+ *          description: A Person object
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Person'
+ */
+router.put('/:id', function(req, res){
+    var query = {'_id': req.params.id};
+
+    var newDetails = {'name': req.body.name, 'location': req.body.location, 'phone': req.body.phone};
+
+    personModel.findOneAndUpdate(query, newDetails, {upsert: true}, function(err, updatedPerson) {
+        if (err) return res.status(500).json({error: err});
+        personModel.findOne(query, '-__v' ,function(err, person) {
+                if (err) {
+                    res.json({"error" : "Internal Sever Error" + err});
+                } else {
+                      if(person == undefined ){
+                         res.status(404).json({"error" : true,"message" : "Record not found"});
+                      }else{
+                         res.json(person);
+                      }
+                }
+            });
+    });
+});
+
 router.delete("/:id", function(req, res){
     let getParam = { _id : req.params.id } ;
     if (req.organisationId !== undefined ) { getParam.organisation = req.organisationId}
 
-    personModel.find(getParam, function(err, person){
+    personModel.findOne(getParam, function(err, person){
         if(err || person == undefined ){
             res.status(404).json({"error" : true,"message" : "Record not found"});
         }else{
